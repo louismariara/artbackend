1b8837caca1de8819f58dd93145dc312437a6adc
Louis Githogori Mariara <louismariara@gmail.com>
2025-02-05 18:09:54 +0300

    render requirements

--- app.py
+++ app.py
@@ -501,4 +501,5 @@ if __name__ == '__main__':
     # Initialize roles before running the app
     with app.app_context():
         initialize_roles()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get port from environment, default to 5000
+    app.run(debug=False, host='0.0.0.0', port=port)
